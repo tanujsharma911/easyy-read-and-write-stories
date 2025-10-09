64c294daf497ef191f2ff5b8db10631dde5cc87b
Tanuj Sharma <hellotanujsharma@gmail.com>
2025-10-09 21:46:44 +0530

    unused k error fixed

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,21 +10,17 @@ export function capitalize(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-type GroupBy<T, K extends keyof T> = Record<string, T[]>;
 
-export function groupBy<T, K extends keyof T>(
+export function groupBy<T, _K extends keyof T>(
   array: T[],
-  key: K
-): GroupBy<T, K> {
+  key: _K
+): Record<string, T[]> {
   return array.reduce((acc, item) => {
     const keyValue = String(item[key]);
-    if (!acc[keyValue]) {
-      acc[keyValue] = [];
-    }
+    if (!acc[keyValue]) acc[keyValue] = [];
     acc[keyValue].push(item);
     return acc;
-  }, {} as GroupBy<T, K>);
+  }, {} as Record<string, T[]>);
 }
 
 export function absoluteUrl(path: string) {
